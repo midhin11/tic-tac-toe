74659158db9c81220ba2e07f703ff7984b49a902
midhin11 <midhincr7@gmail.com>
2026-02-28 23:24:30 +0530

    Create full DOM TicTacToe game
    Created the full tic tac toe game with interactive buttons to go to
    next round or to restart game as well as added conditions to check
    game status and score tracker is included as well.

--- js.js
+++ js.js
@@ -19,46 +19,91 @@ let gameBoard = function () {
 }();
 
 
+function Player(name, marker) {
+    this.name = name;
+    this.marker = marker;
+}
+let player1 = new Player("Player1", "X");
+let player2 = new Player("Player2", "O");
 
-let playGame = function() {
+let board = gameBoard.printBoard();
+let activePlayer = player1;
+let cells = document.querySelectorAll(".cell"); // board selection
+let commentary = document.querySelector(".win-lose-draw"); // Game status Display
+let nextRoundBtn = document.querySelector(".next-round-btn"); // next round button
+let restartBtn = document.querySelector(".restart-btn"); // Restart Button
+nextRoundBtn.disabled = true;
+let isGameOver = false;
 
-    function Player(name, marker) {
-        this.name = name;
-        this.marker = marker;
-    }
-    let player1 = new Player("Player1", "X");
-    let player2 = new Player("Player2", "O");
-    let activePlayer = player1;
-    let board = gameBoard.printBoard();
+// Score display
+let playerOneWins = document.querySelector(".player1 > div:nth-of-type(2)");
+let drawCount = document.querySelector(".draw > div:nth-of-type(2)");
+let playerTwoWins = document.querySelector(".player2 > div:nth-of-type(2)"); 
 
-    
-    while(true){
-        
-        let row = Number(prompt(`${activePlayer.name} with ${activePlayer.marker} Please select row? (0-2)`, 0));
-        let col = Number(prompt(`${activePlayer.name} with ${activePlayer.marker} Please select column? (0-2)`, 0));
+for(let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", function(e) {
+        let row = Number(e.target.dataset.row);
+        let col = Number(e.target.dataset.col);
 
-        let isValid = validityTest(board, row, col);
-        if(isValid) {
-            gameBoard.placeMarker(activePlayer.marker, row, col)
-            displayBoard(board);
-
-            if(checkWin(board, activePlayer) === true) {
-                console.log(`${activePlayer.name} with ${activePlayer.marker} marker wins!`)
-                break;
-            } else if (isBoardFull(board) === true) {
-                console.log(`The Game is a draw!`)
-                break;
-            } else {activePlayer = activePlayer === player1 ? player2 : player1};
+        if(board[row][col] !== "") {
+            return;
         } 
-        
-    }
+
+        if(isGameOver === true) {
+            return;
+        }
+
+        e.target.textContent = activePlayer.marker;
+        board[row][col] = activePlayer.marker;  
+
+        if(checkWin(board, activePlayer)) {
+            commentary.textContent = `${activePlayer.name} with ${activePlayer.marker} wins the Round!`;
+            if(activePlayer === player1) {
+                playerOneWins.textContent = Number(playerOneWins.textContent) + 1;
+            }
+            else {
+                playerTwoWins.textContent = Number(playerTwoWins.textContent) + 1;
+            }
+            nextRoundBtn.disabled = false;
+            isGameOver = true;
+        } 
+        else if(isBoardFull(board)) {
+            commentary.textContent = `The round is a draw!`;
+            isGameOver = true
+            drawCount.textContent = Number(drawCount.textContent) + 1;
+            nextRoundBtn.disabled = false
+        } 
+        else {
+        activePlayer = activePlayer === player1 ? player2 : player1;
+        commentary.textContent = `${activePlayer.name} / ${activePlayer.marker} turn!`
+        }
+    })
 }
 
-playGame();
+nextRoundBtn.addEventListener("click", function(e) {
+    boardLogicClear(board);
+    renderClear(cells);
 
+    activePlayer = player1;
+    nextRoundBtn.disabled = true;
+    commentary.textContent = `${activePlayer.name} / ${activePlayer.marker} turn!`;
+    isGameOver = false;
+})
+
+restartBtn.addEventListener("click", function() {
+    nextRoundBtn.disabled = true;
+    isGameOver = false;
+    activePlayer = player1;
+    commentary.textContent
+    playerOneWins.textContent = 0;
+    playerTwoWins.textContent = 0;
+    drawCount.textContent = 0;
+
+    boardLogicClear(board);
+    renderClear(cells);
+})
 
 //win-check
-
 function checkWin(board, activePlayer) {
     if((board[0][0] === activePlayer.marker && board[1][1] === activePlayer.marker && board[2][2] === activePlayer.marker)
     || (board[0][0] === activePlayer.marker && board[0][1] === activePlayer.marker && board[0][2] === activePlayer.marker)
@@ -77,7 +122,6 @@ function checkWin(board, activePlayer) {
 }
 
 //boardfull check
-
 function isBoardFull(board) {
     let boardArray = [];
     for(let i=0; i<3; i++){
@@ -95,29 +139,19 @@ function isBoardFull(board) {
     return true;
 }
 
-
-//validity test
-
-function validityTest(board, row, col){
-    if(row<0 || row>2 || col<0 || col>2){
-        return false;
-    }
-    else if(board[row][col] != ""){
-        return false;
-    }
-    else return true;
-}
-
-//display-board 
-
-function displayBoard (board) {
-    for (let i=0; i<3; i++){
-        const row = board[i].map(cell => cell === "" ? " " : cell)
-        console.log(row.join(" | "));
-        if(i<2){
-            console.log("----------")
+// Board logic clear 
+function boardLogicClear(board) {
+    for(let i = 0; i<3; i++){
+        for(let j=0; j<3; j++) {
+            board[i][j] = "";
         }
     }
-    console.log("");
-    console.log("");
+}
+
+
+// DOM board clear 
+function renderClear(cells) {
+    for(let i=0; i<cells.length; i++){
+        cells[i].textContent = "";
+    }
 }
